17b6f8c45d1f865b2e2a1da291d0ae522575782b
starsrebirthyou <starsrebirthyou@gmail.com>
2026-01-26 14:12:20 +0900

    Update FollowingIdListService.java

--- blog/src/com/blog/subscribes/service/FollowingIdListService.java
+++ blog/src/com/blog/subscribes/service/FollowingIdListService.java
@@ -5,6 +5,6 @@ public class FollowingIdListService implements Service {
 
     String loginId = (String) obj; // 넘어온 obj를 다시 String 타입으로 변환
 		SubscribesDAO dao = new SubscribesDAO();
-		return dao.followinglist(loginId);
+		return dao.followingList(loginId);
 	}
 }
