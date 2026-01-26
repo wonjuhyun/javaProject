2151c7039218f8e7f4c33793c4acfbd90be6e601
starsrebirthyou <starsrebirthyou@gmail.com>
2026-01-26 14:05:54 +0900

    Update FollowingIdListService.java

--- blog/src/com/blog/subscribes/service/FollowingIdListService.java
+++ blog/src/com/blog/subscribes/service/FollowingIdListService.java
@@ -1,9 +1,9 @@
 public class FollowingIdListService implements Service {
 
 	@Override
-	public Object service(Object ibj) throws Exception {
+	public Object service(Object obj) throws Exception {
 
-    String loginId (String) obj; // 넘어온 obj를 다시 String 타입으로 변환
+    String loginId = (String) obj; // 넘어온 obj를 다시 String 타입으로 변환
 		SubscribesDAO dao = new SubscribesDAO();
 		return dao.list(loginId);
 	}
