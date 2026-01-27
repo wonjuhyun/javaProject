ae7e60905b2552079f999d4b9d0e1a82e916a627
user <user@DESKTOP-8496UEN>
2026-01-27 15:34:58 +0900

    MemberService 2차 수정

--- blog/src/com/blog/member/service/LoginService.java
+++ blog/src/com/blog/member/service/LoginService.java
@@ -1,14 +1,14 @@
 package com.blog.member.service;
 
 import com.blog.main.service.Service;
+import com.blog.member.dao.MemberDAO;
+import com.blog.member.vo.MemberVO;
 
 public class LoginService implements Service {
 
-	@Override
-	public Object service(Object obj) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-
+    @Override
+    public Object service(Object obj) throws Exception {
+        MemberVO vo = (MemberVO) obj;
+        return new MemberDAO().login(vo);
+    }
 }
--- blog/src/com/blog/member/service/MemberSearchIdService.java
+++ blog/src/com/blog/member/service/MemberSearchIdService.java
@@ -1,13 +1,16 @@
 package com.blog.member.service;
 
 import com.blog.main.service.Service;
+import com.blog.member.dao.MemberDAO;
+import com.blog.member.vo.MemberVO;
 
 public class MemberSearchIdService implements Service{
 
 	@Override
 	public Object service(Object obj) throws Exception {
 		// TODO Auto-generated method stub
-		return null;
-	}
+
+		MemberDAO dao = new MemberDAO();
+		return dao.searchId((MemberVO) obj);	}
 
 }
