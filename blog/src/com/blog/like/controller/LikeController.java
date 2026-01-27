7a59b0efa7eea4b84a0383d0136c4aa3b97d39a9
yeonoh2402596 <jooyeonoh3526777@gmail.com>
2026-01-27 17:27:37 +0900

    LikeController 생성 완료

---
+++ blog/src/com/blog/like/controller/LikeController.java
@@ -0,0 +1,35 @@
+package com.blog.like.controller;
+
+import com.blog.board.vo.BoardVO;
+import com.blog.board.vo.CurrentBoard;
+import com.blog.like.dao.LikeDAO;
+import com.blog.like.service.LikeDeleteService;
+import com.blog.like.service.LikeInsertService;
+import com.blog.like.vo.LikeVO;
+import com.blog.main.service.Execute;
+//import com.blog.member.vo.Login;
+
+import a.Login;
+
+
+public class LikeController {
+
+	public void execute() throws Exception {
+		LikeVO vo = new LikeVO();
+		LikeDAO like = new LikeDAO();
+		BoardVO board = new BoardVO();
+
+		vo.setPostNo(CurrentBoard.getBoardVO().getPostNo()); 
+		vo.setLikerId(Login.getId());
+		
+		if(like.isLiked(vo)) {
+			Execute.execute(new LikeInsertService(), vo);
+			System.out.println("*** '" + board.getWriterNick() + "'님의 게시글에 공감을 누르셨습니다. ***");
+		} else {
+			Execute.execute(new LikeDeleteService(), vo);
+			System.out.println("*** '" + board.getWriterNick() + "'님의 게시글에 공감을 취소하셨습니다. ***");
+		}
+
+	}
+	
+}
