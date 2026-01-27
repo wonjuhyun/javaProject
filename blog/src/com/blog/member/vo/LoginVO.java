a9c69babe34d0f13c143edb5372f2b10140bb32e
leehw <leehw@Voyager>
2026-01-27 17:47:32 +0900

    LoginVO 수정

--- blog/src/com/blog/member/vo/LoginVO.java
+++ blog/src/com/blog/member/vo/LoginVO.java
@@ -2,16 +2,46 @@ package com.blog.member.vo;
 
 public class LoginVO {
 
-    private String id;
-    private String name;
-    private String nickname;
+	private String id;
+	private String pw;
+	private String name;
+	private String nickname;
 
-    public String getId() { return id; }
-    public void setId(String id) { this.id = id; }
+	public String getId() {
+		return id;
+	}
 
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
+	public void setId(String id) {
+		this.id = id;
+	}
 
-    public String getNickname() { return nickname; }
-    public void setNickname(String nickname) { this.nickname = nickname; }
+	public String getPw() {
+		return pw;
+	}
+
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	@Override
+	public String toString() {
+		return "LoginVO [id=" + id + ", pw=" + pw + ", name=" + name + ", nickname=" + nickname + "]";
+	}
+
 }
